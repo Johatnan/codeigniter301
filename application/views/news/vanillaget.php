db1b43ba36360d24e0b1e965cadccc9de804f82a
Johatnan <84165800+Johatnan@users.noreply.github.com>
2021-06-23 16:19:52 +0800

    Enter

---
+++ application/views/news/vanillaget.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <script>
+                const request = new XMLHttpRequest();
+                const url = 'https://codeigniter301.herokuapp.com/index.php/newsapi/';
+                request.open("GET", url);
+                request.send();
+
+                request.onload = (e) => {
+                    alert(request.response);
+                }
+        </script>
+    </body>
+</html>
