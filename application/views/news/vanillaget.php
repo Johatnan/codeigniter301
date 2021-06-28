262a2b76ed49e32a5ea9a5b0730780a26b9e3e84
Johatnan <84165800+Johatnan@users.noreply.github.com>
2021-06-28 14:43:04 +0800

    asdf

--- application/views/news/vanillaget.php
+++ application/views/news/vanillaget.php
@@ -6,7 +6,7 @@
     <body>
         <script>
                 const request = new XMLHttpRequest();
-                const url = 'https://codeigniter301.herokuapp.com/index.php/newsapi/';
+                const url = 'https://codeigniter333.herokuapp.com/index.php/newsapi/';
                 request.open("GET", url);
                 request.send();
 
